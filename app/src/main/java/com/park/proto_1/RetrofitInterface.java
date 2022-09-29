7e9809f21327c972168b483bcc4c0c1953130f97
OHT <htoh55@naver.com>
2022-09-29 18:46:43 +0900

    image complete.1

--- app/src/main/java/com/park/proto_1/RetrofitInterface.java
+++
@@ -1,9 +0,0 @@
-package com.park.proto_1;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface RetrofitInterface {
-    @GET("/AbdmAnimalProtect?KEY=25af84057280489d90e4a25dd9138611&Type=json&pIndex=1&pSize=10&SIGUN_CD=41590&SPECIES_NM=[개]")
-    Call<ApiDTO> getAllData();
-}
